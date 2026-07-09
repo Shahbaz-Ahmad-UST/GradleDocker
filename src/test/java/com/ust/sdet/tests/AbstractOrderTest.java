f056afcec93334d93ecc5a443fdc02da52ee23cb
Shahbaz Ahmad(UST <307424@ust.com>
2026-07-09 10:25:19 +0530

    commit 8

--- src/test/java/com/ust/sdet/tests/AbstractOrderTest.java
+++ src/test/java/com/ust/sdet/tests/AbstractOrderTest.java
@@ -5,64 +5,57 @@ import com.ust.sdet.factory.OrderFactory;
 import com.ust.sdet.repository.OrderRepository;
 import com.ust.sdet.support.DbSupport;
 import com.ust.sdet.support.TestEnvironment;
-import io.cucumber.java.ja.且つ;
 import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public abstract class AbstractOrderTest {
 
-    @Container
-    static final MySQLContainer<?> MYSQL = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("orders_db")
-            .withUsername(TestEnvironment.required("DB_USER"))
-            .withPassword(TestEnvironment.required("DB_PASSWORD"));
-
+    static MySQLContainer<?> mysql;
     static DatabaseConfig config;
     static OrderRepository repository;
     static OrderFactory factory;
     static DbSupport dbSupport;
 
-
     @BeforeAll
     static void setupDatabase() {
+        boolean useContainer = Boolean.parseBoolean(TestEnvironment.optional("USE_TESTCONTAINERS", "false"));
 
-        String useContainer = TestEnvironment.required("USE_TESTCONTAINERS");
-
-        if ("true".equalsIgnoreCase(useContainer)) {
-
-            MYSQL.start();
-
-            config = DatabaseConfig.fromContainer(
-                    MYSQL.getJdbcUrl() + "?allowPublicKeyRetrieval=true&useSSL=false",
-                    MYSQL.getUsername(),
-                    MYSQL.getPassword()
-            );
-        } else {
+        if (useContainer) {
+            mysql = new MySQLContainer<>("mysql:8.0")
+                    .withDatabaseName("orders_db")
+                    .withUsername("test")
+                    .withPassword("test");
+            mysql.start();
 
             config = new DatabaseConfig(
-                    TestEnvironment.required("DB_JDBC_URL"),
-                    TestEnvironment.required("DB_USER"),
-                    TestEnvironment.required("DB_PASSWORD")
+                    mysql.getJdbcUrl() + "?allowPublicKeyRetrieval=true&useSSL=false",
+                    mysql.getUsername(),
+                    mysql.getPassword()
             );
+        } else {
+            config = DatabaseConfig.fromEnvironmentCredential();
         }
 
-        Flyway flyway = Flyway.configure()
+        Flyway.configure()
                 .dataSource(config.jdbcUrl(), config.username(), config.password())
                 .locations("classpath:db/migration")
-                .load();
-
-        flyway.migrate();
+                .load()
+                .migrate();
 
         repository = new OrderRepository(config);
         factory = new OrderFactory(repository);
         dbSupport = new DbSupport(config);
     }
 
+    @AfterAll
+    static void tearDownDatabase() {
+        if (mysql != null) {
+            mysql.stop();
+        }
+    }
 
     @BeforeEach
     void resetDatabase() {
